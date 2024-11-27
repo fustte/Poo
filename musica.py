0bbabf7b9b315bb12c3405e650d9720f865dcbe5
Fustee <chico_jrf@hotmail.com>
2024-11-27 19:53:21 +0100

    feat: creo clases

---
+++ musica.py
@@ -0,0 +1,26 @@
+class Intrumento:
+    pass
+
+
+class Vientos(Instrumento):
+    pass
+
+
+class Percusion(Instrumento):
+    pass
+
+
+class Cuerda(Intrumento):
+    pass
+
+
+class Pulsada(Cuerda):
+    pass
+
+
+class Frotada(Cuerda):
+    pass
+
+
+class Golpeada(Cuerda):
+    pass
