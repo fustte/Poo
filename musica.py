370f516b49b0569bdfc7a8082b0a8ba042faa649
Fustee <chico_jrf@hotmail.com>
2024-11-28 13:07:17 +0100

    pausa cambio a portatil

--- musica.py
+++ musica.py
@@ -1,7 +1,9 @@
 
 class Orquesta:
 
-    def __init__(self):
+    def __init__(self, nombre= ''):
+
+
         self.componentes = []  # lista de intrumentos
 
     def agregarInstrumento(self, instrumento):
@@ -12,6 +14,15 @@ class Orquesta:
     def listarInstrumentos(self):
         for instrumento in self.componentes:
             print(instrumento)
+
+    def interpretar(self, obra=''):
+        if obra != '':
+            print('-'*60)
+            print('\tInterpretando:', obra)
+            print('-'*60)
+
+        for instrumento in self.componentes:
+            instrumento.sonar()
 
 class Instrumento:
 
@@ -29,12 +40,20 @@ class Instrumento:
         return self.nombre
 
 
-class Vientos(Instrumento):
-    pass
+class Viento(Instrumento):
+
+    def sonar(self):
+        print('sopla', end= ' ')
+        super().sonar()
+
 
 
 class Percusion(Instrumento):
-    pass
+    
+    def sonar (self):
+        print('golpea', end= ' ')
+        super().sonar()
+    
 
 
 class Cuerda(Instrumento):
@@ -59,13 +78,23 @@ if __name__ == '__main__':
     orquesta = Orquesta()
     
     piano = Golpeada('primer piano')
+    piano.sonido = 'pin pin pin'
     violin = Frotada('violin 1')
+    violin.sonido = 'flic flic flic'
     vn = Frotada('violin 2')
+    vn.sonido = 'flac flac flac'
     timbal = Percusion('timbal principal')
+    timbal.sonido = 'pum pum pum'
+    fagot = Viento('fagot alto')
+    fagot.sonido = 'fiu fiu fiu'
 
     orquesta.agregarInstrumento(piano)
     orquesta.agregarInstrumento(violin)
     orquesta.componentes.append(vn)
     orquesta.componentes.append(timbal)
+    orquesta.componentes.append(fagot)
 
-    orquesta.listarInstrumentos()
+    #orquesta.listarInstrumentos()
+    
+    for instrumento in orquesta.componentes:
+        instrumento.sonar()
