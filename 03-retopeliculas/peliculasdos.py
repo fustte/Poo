34144d250912fcf8fdb4002b0316256825b4e0c3
Fustee <chico_jrf@hotmail.com>
2024-11-30 06:50:33 +0100

    feat: "Copia codigo peliculas como base"

---
+++ 03-retopeliculas/peliculasdos.py
@@ -0,0 +1,31 @@
+from datetime import date
+
+class show:
+    def __init__(self, titulo, tipo):
+        self.titulo = titulo
+        self.tipo = tipo
+        self.terminada = False
+        self.fecha_entrada = date.today()
+
+    def marcar_terminada(self):
+        self.terminada = True
+
+    
+    def __str__(self):
+        vista = "Si" if self.terminada else "No"
+        return f"Títutlo: {self.titulo}\tTipo: {self.tipo}\tVisto: {vista}\tFecha de Entrada: {self.fecha_entrada}"
+    
+mis_pelis = [
+
+        show('Star Wars', 'film'),
+        show('Avatar 2', 'film'),
+        show('Wormwood', 'doc'),
+        show('The Wire', 'serie')
+    
+    ]
+
+for show in mis_pelis:
+    if show.titulo == "The Wire":
+        show.marcar_terminada()
+
+for show in mis_pelis:
