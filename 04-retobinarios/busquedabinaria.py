1b6b0124b192146f14eda618f29d7395daed9298
Fustee <chico_jrf@hotmail.com>
2024-12-03 09:18:29 +0100

    feat: creo funcion insertar

--- 04-retobinarios/busquedabinaria.py
+++ 04-retobinarios/busquedabinaria.py
@@ -1,3 +1,4 @@
+# Creo clase Nodo
 
 class Nodo:
     def __init__(self, valor):
@@ -7,3 +8,32 @@ class Nodo:
 
     def __repr__(self):
         return str(self.valor)
+    
+class Arbol:
+    def __init__(self):
+        self.raiz = None
+        self.coste_ultima_busqueda = 0
+
+    def insertar(self, valor):
+        if self.raiz is None:
+            self.raiz = Nodo(valor)
+        else:
+            self._insertar_recursivo(self.raiz, valor)
+
+    def _insertar_recursivo(self, nodo valor):
+        if valor < nodo.valor:
+            if nodo.izquierdo is None:
+                nodo.izquierdo = Nodo(valor)
+
+            else:
+                self._insertar_recursivo(nodo.izquierdo, valor)
+    
+    def _insertar_recursivo(self, nodo valor):
+        if valor < nodo.valor:
+            if nodo.derecho is None:
+                nodo.derecho = Nodo(valor)
+
+            else:
+                self._insertar_recursivo(nodo.derecho, valor)
+
+    
