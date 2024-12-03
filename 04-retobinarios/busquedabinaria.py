ad564bf62dd3628774ae8bf313118e657361d912
Fustee <chico_jrf@hotmail.com>
2024-12-03 09:30:31 +0100

    feat: Creo funcion eliminar

--- 04-retobinarios/busquedabinaria.py
+++ 04-retobinarios/busquedabinaria.py
@@ -52,4 +52,26 @@ class Arbol:
         else:
             return self._buscar_recursivo(nodo.derecho, valor)
         
+    def eliminar(self, valor):
+        self.raiz = self._eliminar_recursivo(self.raiz, valor)
+
+    def _eliminar_recursivo(self, nodo, valor):
+        if nodo is None:
+            return nodo
         
+        if valor < nodo.valor:
+            nodo.izquierdo = self._eliminar_recursivo(nodo.izquierdo, valor)
+        elif valor < nodo.valor:
+            nodo.derecho = self._ekiminar_recursivo(nodo.derecho, valor)
+
+        else:
+            if nodo.izquierdo is None:
+                return nodo.derecho
+            elif nodo.derecho is None:
+                return nodo.izquierdo
+            nodo.valor = self._minimo_valor(nodo.derecho)
+            nodo.derecho = self._eliminar_recursivo(nodo.derecho, nodo.valor)
+
+        return nodo
+    
+    
